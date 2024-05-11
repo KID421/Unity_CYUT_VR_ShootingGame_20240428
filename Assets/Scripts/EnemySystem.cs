4336770699c621cba92f2b1739a7735a5a079038
KID <50666207+KID421@users.noreply.github.com>
2024-05-11 16:26:37 +0800

    Update 添加敵人系統

---
+++ Assets/Scripts/EnemySystem.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemySystem : MonoBehaviour
+{
+    [SerializeField, Header("玩家")]
+    private Transform playerPoint;
+    [SerializeField, Header("導覽代理器")]
+    private NavMeshAgent agent;
+
+    private void Update()
+    {
+        // 代理器 的 設定目的地 (玩家的座標)
+        agent.SetDestination(playerPoint.position);
+    }
+}
