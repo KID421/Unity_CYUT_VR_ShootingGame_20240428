8a924c467b78f1bc027090b0addf13ff113b4c84
KID <50666207+KID421@users.noreply.github.com>
2024-05-12 11:59:34 +0800

    Update 添加攻擊方法

--- Assets/Scripts/EnemySystem.cs
+++ Assets/Scripts/EnemySystem.cs
@@ -12,10 +12,12 @@ public class EnemySystem : MonoBehaviour
 
     private string parMove = "移動數值";
     private string parAttack = "觸發攻擊";
+    private bool isAttacking;
 
     private void Update()
     {
         Move();
+        Attack();
     }
 
     private void Move()
@@ -25,5 +27,20 @@ public class EnemySystem : MonoBehaviour
 
         // 動畫控制器 設定浮點數(參數名稱，浮點數)
         ani.SetFloat(parMove, agent.velocity.magnitude / agent.speed);
+    }
+
+    private void Attack()
+    {
+        // 如果 正在攻擊中 就跳出
+        if (isAttacking) return;
+
+        // print($"<color=#6f9>距離：{agent.remainingDistance}</color>");
+
+        // 如果 剩餘距離 <= 停止距離 就攻擊
+        if (agent.remainingDistance <= agent.stoppingDistance)
+        {
+            ani.SetTrigger(parAttack);
+            isAttacking = true;
+        }
     }
 }
